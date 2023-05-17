ed8f0a3d3b6b583f419c9f315810f183d8aabbeb
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2023-05-17 18:42:59 -0400

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -6,4 +6,4 @@ DEST=$1
 DIR="/usr/src/tv-led-strip"
 
 rsync -avh -e "ssh -i $HOME/.ssh/rpi.key" --rsync-path="sudo rsync" . $DEST:$DIR
-ssh -i ~/.ssh/rpi.key -t $DEST "cd $DIR && /usr/local/go/bin/go install -v"
+ssh -i ~/.ssh/rpi.key -t $DEST "cd $DIR && /usr/local/go/bin/go install -v && sudo cp tv-led-strip.service /lib/systemd/system && sudo systemctl daemon-reload"
